72a90854e73a8f113597a0c569ed0ff8c563a58b
tarde <tarde@G24-EQ14>
2019-11-25 20:55:29 +0100

    Commit 25/11/2019 clase

--- 25_microservicios_reservas/src/main/java/controller/ReservaController.java
+++ 25_microservicios_reservas/src/main/java/controller/ReservaController.java
@@ -1,40 +1,39 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import model.Reserva;
 import service.ServiceReservas;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins="*")
 @RestController
 public class ReservaController {
-
 	@Autowired
-	ServiceReservas sReservas;	
-	
-	@Value("${client.url.inventada}")
-	String url;
-	
+	ServiceReservas serviceReservas;
 	@Autowired
 	RestTemplate template;
 	
-	@PostMapping(value = "/reservar/{idVuelo}/{idHotel}/{dni}/{nombre}/{plazas}")
-	void reservar(@PathVariable("idVuelo") int idVuelo,
-				  @PathVariable("idHotel") int idHotel,
-				  @PathVariable("dni") String dni,
-				  @PathVariable("nombre") String nombre,
-				  @PathVariable("plazas") int plazas) {
-		Reserva reserva= new Reserva();
-		reserva.setVuelo(idVuelo);
-		reserva.setHotel(idHotel);
-		reserva.setDni(dni);
-		reserva.setNombre(nombre);
-		sReservas.reservar(reserva);
+	@Value("${url.servicio.vuelos}")
+	String url;
+	
+	@PostMapping(value="/reservar", consumes = MediaType.APPLICATION_JSON_VALUE)
+	void reservar(@RequestBody Reserva reserva) {
+		serviceReservas.reservar(reserva);
+		template.put(url+"/"+reserva.getVuelo()+"/"+reserva.getPlazas(), null);
+	}
+	
+	@GetMapping(value="/lista",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Reserva> listarReservas() {
+		return serviceReservas.listar();
 	}
 }
---
+++ 25a_microservicios_reservas_config_cloud/src/main/java/controller/ReservaController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import model.Reserva;
+import service.ServiceReservas;
+
+@CrossOrigin(origins="*")
+@RestController
+public class ReservaController {
+	@Autowired
+	ServiceReservas serviceReservas;
+	@Autowired
+	RestTemplate template;
+	
+	@Value("${url.servicio.vuelos}")
+	String url;
+	
+	@PostMapping(value="/reservar", consumes = MediaType.APPLICATION_JSON_VALUE)
+	void reservar(@RequestBody Reserva reserva) {
+		System.out.println("xxx" + url);
+		System.out.println("xxx"+ reserva);
+		serviceReservas.reservar(reserva);
+		template.put(url+"/"+reserva.getVuelo()+"/"+reserva.getPlazas(), null);
+	}
+	
+	@GetMapping(value="/lista",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Reserva> listarReservas() {
+		return serviceReservas.listar();
+	}
+}
